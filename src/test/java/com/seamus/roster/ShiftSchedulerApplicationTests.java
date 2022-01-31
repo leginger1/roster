81fe53ab6102f5e378ba05087570cf3abdb9932e
leginger <seamus.tim@gmail.com>
2022-01-31 18:56:53 +0000

    Initial commit

---
+++ src/test/java/com/seamus/roster/ShiftSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seamus.roster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
